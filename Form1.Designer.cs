d62a5fc0ca6dc43401dcce6398eabc2fa7065c3f
Ruslan <abishev2012@gmail.com>
2023-10-23 15:37:44 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PingLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbIPAddresses = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbIPAddresses
+            // 
+            this.tbIPAddresses.Location = new System.Drawing.Point(24, 30);
+            this.tbIPAddresses.Multiline = true;
+            this.tbIPAddresses.Name = "tbIPAddresses";
+            this.tbIPAddresses.Size = new System.Drawing.Size(187, 68);
+            this.tbIPAddresses.TabIndex = 0;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(24, 104);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbResult.Size = new System.Drawing.Size(383, 197);
+            this.tbResult.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(302, 42);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(24, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "введите имя либо ip адрес";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(247, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "выберите количество пингов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 369);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbIPAddresses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbIPAddresses;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+using Timer = System.Timers.Timer;
+using System.Collections.Generic;
+
+namespace PingLab
+{
+    public partial class Form1 : Form
+    {
+        IPAddress[] ips;
+        Timer timer;
+        int packetSize = 1024; //  размер пакета ICMP
+        string logFileName = "log.txt";
+        int pingCount;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer { Interval = 2000 }; // Интервал в миллисекундах (2 секунды)
+            timer.Elapsed += TimerElapsed;
+        }
+
+        Dictionary<IPAddress, int> pingCounter = new Dictionary<IPAddress, int>();
+
+        void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            foreach (var ip in ips)
+            {
+                if (!pingCounter.ContainsKey(ip))
+                {
+                    pingCounter[ip] = 0; // инициализируем счетчик
+                }
+
+                if (pingCounter[ip] < pingCount)
+                {
+                    var result = MyPing(ip, packetSize);
+                    LogToFile(result);
+                    pingCounter[ip]++; 
+                }
+            }
+        }
+
+        string MyPing(IPAddress ip, int bufferSize)
+        {
+            string result = string.Empty;
+            string br = "\r\n";
+            string hostName = string.Empty;
+            Ping ping = new Ping();
+            PingOptions pingOptions = new PingOptions();
+            pingOptions.DontFragment = true;
+
+            try
+            {
+                byte[] buffer = new byte[bufferSize];
+
+                hostName = Dns.GetHostEntry(ip).HostName;
+                PingReply pingReply = ping.Send(ip, (int)timer.Interval, buffer, pingOptions);
+                if (pingReply.Status == IPStatus.Success)
+                {
+                    result = $"Узел {hostName} ({ip}) доступен {DateTime.Now.ToString("HH:mm:ss.fff")}, Размер пакета: {bufferSize}, Частота проверки: {timer.Interval / 1000.0} seconds.{br}";
+                }
+                else
+                {
+                    result = $"Узел {hostName} ({ip}) НЕ ДОСТУПЕН {DateTime.Now.ToString("HH:mm:ss.fff")}, Размер пакета: {bufferSize}, Частота проверки: {timer.Interval / 1000.0} seconds.{br}";
+                }
+            }
+            catch (Exception ex)
+            {
+                result = $"ОШИБКА: {ex.Message}{br}";
+            }
+
+            this.Invoke((MethodInvoker)delegate {
+                tbResult.Text += result + Environment.NewLine;
+            });
+
+            return result;
+        }
+
+        void LogToFile(string message)
+        {
+            File.AppendAllText(logFileName, message);
+        }
+
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            pingCount = (int)numericUpDown1.Value;
+            pingCounter.Clear(); // очищаем счетчик
+
+            List<IPAddress> ipList = new List<IPAddress>();
+
+            foreach (var host in tbIPAddresses.Lines)
+            {
+                if (string.IsNullOrWhiteSpace(host))
+                    continue;
+
+                try
+                {
+                    var ips = Dns.GetHostEntry(host).AddressList[0];
+                    ipList.Add(ips);
+                }
+                catch (SocketException)
+                {
+                    MessageBox.Show($"Не удалось разрешить хост: {host}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  
+                }
+            }
+
+            ips = ipList.ToArray();
+            timer.Start();
+        }
+    }
+
+  
+}
